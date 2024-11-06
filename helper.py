bb06a7cd000dd70af5a99d593df05702cc86850c
barbaragrosjean <barbara.grosjean@epfl.ch>
2024-11-06 07:18:30 +0100

    try to add temporal PCA just to see + drift

--- helper.py
+++ helper.py
@@ -13,3 +13,6 @@ def compute_FD_power(mot_params):
     trans_params=framewise_diff[['Translation X', 'Translation Y', 'Translation Z']]
     fd=converted_rots.abs().sum(axis=1)+trans_params.abs().sum(axis=1)
     return fd 
+
+def namestr(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
